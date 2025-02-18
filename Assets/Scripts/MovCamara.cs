5c332d22dd69cc738fbdea476f98a7887ce35988
MauAlcantara1 <japonxd1@hotmail.com>
2025-02-18 17:41:13 -0600

    Actualizacion 2 de la primera version

--- Assets/Scripts/MovCamara.cs
+++ Assets/Scripts/MovCamara.cs
@@ -26,19 +26,19 @@ public class MovCamara : MonoBehaviour
         if(obj.gameObject.tag == "portal4"){
             Vector3 posicioncamara = new Vector3(4,0,-10);
             camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(1,1,1);
+            Vector3 posicionPlayer = new Vector3(-3.56f, -8,0);
             this.transform.position = posicionPlayer;
         }  
         if(obj.gameObject.tag == "portal5"){
             Vector3 posicioncamara = new Vector3(4,0,-10);
             camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(1,1,1);
+            Vector3 posicionPlayer = new Vector3(18.5f,1,0);
             this.transform.position = posicionPlayer;
         } 
         if(obj.gameObject.tag == "portal6"){
             Vector3 posicioncamara = new Vector3(4,0,-10);
             camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(1,1,1);
+            Vector3 posicionPlayer = new Vector3(-4,7.9f,0);
             this.transform.position = posicionPlayer;
         } 
     }
