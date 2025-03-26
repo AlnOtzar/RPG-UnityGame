7a16ac1cc7e580005028529faaaaced1754dc24a
MauAlcantara1 <japonxd1@hotmail.com>
2025-03-26 17:30:21 -0600

    Cambios

--- Assets/Scripts/MovCamara.cs
+++ Assets/Scripts/MovCamara.cs
@@ -8,7 +8,7 @@ public class MovCamara : MonoBehaviour
         if(obj.gameObject.tag == "portal1"){
             //Vector3 posicioncamara = new Vector3(4,20,-10);
             //camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(-94,74,0);
+            Vector3 posicionPlayer = new Vector3(-130,149,0);
             this.transform.position = posicionPlayer;
         } 
         if(obj.gameObject.tag == "portal2"){
@@ -20,19 +20,19 @@ public class MovCamara : MonoBehaviour
         if(obj.gameObject.tag == "portal3"){
             //Vector3 posicioncamara = new Vector3(80,0,-10);
             //camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(-144,-108,0);
+            Vector3 posicionPlayer = new Vector3(-180,-33,0);
             this.transform.position = posicionPlayer;
         }
         if(obj.gameObject.tag == "portal4"){
             //Vector3 posicioncamara = new Vector3(4,0,-10);
             //camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(-90, -138,0);
+            Vector3 posicionPlayer = new Vector3(-126, -63,0);
             this.transform.position = posicionPlayer;
         }  
         if(obj.gameObject.tag == "portal5"){
             //Vector3 posicioncamara = new Vector3(4,0,-10);
             //camara.transform.position = posicioncamara;
-            Vector3 posicionPlayer = new Vector3(-192,-108,0);
+            Vector3 posicionPlayer = new Vector3(-230,-33,0);
             this.transform.position = posicionPlayer;
         } 
         if(obj.gameObject.tag == "portal6"){
